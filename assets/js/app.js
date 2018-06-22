3e4d05da57b3bf875548b146f8af4893b70dfe13
Alex Camacho <alex.c@WalkMe.local>
2018-06-21 21:23:16 -0700

    Making sure we're grabbing the search fields for the ajax query string

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-var begindate = $('').val();
-var enddate = $('').val();
-var query = $('').val();
+var begindate = $('#searchParameters [name=start]').val().trim();
+var enddate = $('#searchParameters [name=end]').val().trim();
+var query = $('#searchParameters [name=searchTerm]').val().trim();
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=44e19de4662d445db0a2832a07549a29";
 if (begindate !== '') url += "&begin_date=" + begindate;
